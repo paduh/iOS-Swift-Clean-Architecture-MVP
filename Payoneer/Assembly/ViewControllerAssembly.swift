7677e6b1b978e5885882dff33be229b5daf7c1cd
Perfect Aduh <paduh@getcarcon.co>
2021-06-06 10:33:34 +0100

    implemented view controller assembly

---
+++ Payoneer/Assembly/ViewControllerAssembly.swift
@@ -0,0 +1,14 @@
+//
+//  ViewControllerAssembly.swift
+//  Payoneer
+//
+//  Created by Aduh Perfect on 06/06/2021.
+//
+
+import Foundation
+
+// MARK:- ViewController Assembly
+
+enum ViewControllerAssembly {
+    
+}
---
+++ Payoneer/PaymenMethods/PaymentMethodsAssembly/Assembly+PaymenMethods.swift
@@ -0,0 +1,17 @@
+//
+//  Assembly+PaymenMethods.swift
+//  Payoneer
+//
+//  Created by Aduh Perfect on 06/06/2021.
+//
+
+import UIKit
+
+// MARK:- Payment methods assembly
+
+extension ViewControllerAssembly {
+    
+    static var paymentMethodsController: UIViewController {
+        UIViewController()
+    }
+}
